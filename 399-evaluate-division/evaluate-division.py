25bb878120a5df734d8d946eac8000a7df867632
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2025-01-16 06:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (24.52%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,31 +1,28 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(list)
-        n = len(equations)
-        for i in range(n):
-            equation = equations[i]
-            graph[equation[0]].append([equation[1], values[i]])
-            graph[equation[1]].append([equation[0], 1/values[i]])
+        # graph 
+        def dfs(start, target):
+            if start == target:
+                return 1.0
+            for neighbour, val in graph[start]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    temp = dfs(neighbour, target)
+                    if temp != -1.0:
+                        return val*temp
+            return -1.0
+
+        graph = defaultdict(set)
+        for i, [a,b] in enumerate(equations):
+            graph[a].add((b,values[i]))
+            graph[b].add((a, 1/values[i]))
+        
         ans = []
         for query in queries:
-            ans.append(self.bfs(query, graph))
-
-        return  ans
-
-    def bfs(self, query, graph):
-        start, dest = query[0], query[1]
-        if start not in graph or dest not in graph:
-            return -1.0
-        queue = deque()
-        visited =set()
-        queue.append([start, 1])
-        while (len(queue) > 0):
-            [node, value] = queue.popleft()
-            if node == dest:
-                return value
-            for neighbour in graph[node]:
-                if neighbour[0] not in visited:
-                    visited.add(neighbour[0])
-                    queue.append([neighbour[0], neighbour[1]*value])
-        return -1.0
-                
+            if query[0] not in graph or query[1] not in graph:
+                ans.append(-1.0)
+                continue
+            visited = set()
+            visited.add(query[0])
+            ans.append(dfs(query[0], query[1]))
+        return ans
