0afc6e43e8746bdceb0ec28f8567a5db3f35f605
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2025-01-25 12:24:00 +0530

    Time: 7 ms (51.56%) | Memory: 19.1 MB (50.69%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,21 +1,25 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        par1 = self.find(x)
+        par2 = self.find(y)
+        if par1==par2:
+            return False
+        self.parent[par1] = par2
+        return True
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(set)
+        components = n
+        dsu = DSU(n)
         for edge in edges:
-            graph[edge[0]].add(edge[1])
-            graph[edge[1]].add(edge[0])
-        visited = set()
-        count = 0
-        for i in range(n):
-            if i not in visited:
-                visited.add(i)
-                self.dfs(i, visited, graph)
-                count+=1
-        return count
-    
-    def dfs(self, i, visited, graph):
-        for neighbour in graph[i]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                self.dfs(neighbour, visited, graph)
-    
+            if dsu.union(edge[0], edge[1]):
+                components-=1
+        return components
