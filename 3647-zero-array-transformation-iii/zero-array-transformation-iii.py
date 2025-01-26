40189e1b2071159b96612eab3bd947e5f32cb2f0
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2025-01-27 00:09:11 +0530

    Time: 374 ms (27.32%) | Memory: 62.3 MB (17.57%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
@@ -1,22 +1,27 @@
 class Solution:
     def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        chosen = []
         queries.sort()
         candidates = []
-        chosen = []
-        ans, n, j = 0, len(nums), 0
+        n = len(nums)
+        j, ans = 0, 0
         for i in range(n):
             while j<len(queries) and queries[j][0] == i:
+                # we prioritize the range query with the furthest ending point
                 heapq.heappush(candidates, -queries[j][1])
                 j+=1
-            nums[i]-=len(chosen)
-            while nums[i] > 0 and candidates and -candidates[0] >=i:
+            nums[i] -= len(chosen)
+            while nums[i] > 0 and candidates and -candidates[0]>=i:
                 ans+=1
+                #chosen needs to keep track of which queries have ended and remove them
+                # hence the minHeap
                 heapq.heappush(chosen, -heapq.heappop(candidates))
                 nums[i]-=1
-            if nums[i]>0:
+            if nums[i] >0:
                 return -1
-            while chosen and chosen[0] <= i:
+            while chosen and chosen[0] <=i:
                 heapq.heappop(chosen)
         return len(queries) - ans
-
             
+
+
