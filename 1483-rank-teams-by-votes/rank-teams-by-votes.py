d112c8fefb3f4f875c5b4102e5219664d85a077d
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2025-01-28 11:20:05 +0530

    Time: 16 ms (32.20%) | Memory: 18.2 MB (19.79%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.py
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.py
@@ -2,9 +2,10 @@ class Solution:
     def rankTeams(self, votes: List[str]) -> str:
         countMap = {}
         for vote in votes:
-            for rank,candidate in enumerate(vote):
+            for rank, candidate in enumerate(vote):
                 if candidate not in countMap:
-                    countMap[candidate] = [0]*len(vote) # votes for each rank for the candidate
+                    countMap[candidate] = [0]*len(vote)
                 countMap[candidate][rank]+=1
         votedNames = sorted(countMap.keys())
-        return "".join(sorted(votedNames, key = lambda x:countMap[x], reverse = True))
+        arr = sorted(votedNames, key = lambda x: countMap[x], reverse=True)
+        return "".join(arr)
