5532d91ca25129f79c05fa4e9ef06ab3087cede5
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2025-01-31 18:06:09 +0530

    Time: 366 ms (53.81%) | Memory: 19.5 MB (58.43%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,18 +1,17 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-      #graph, dijkstra cos weights
-      m, n= len(heights), len(heights[0])
-      distances = [[float('inf')]*n for i in range(m)]
-      distances[0][0] = 0
-      queue = [(0,0, 0)] #(row, col, dist)
-      neighbours=  {(0,1), (0, -1), (1, 0), (-1, 0)}
-      while queue:
-        r, c, dist = queue.pop(0)
-        if r==m-1 and c==n-1:
-            continue
-        for neighbour in neighbours:
-            newr, newc = r+neighbour[0], c+neighbour[1]
-            if 0<=newr<m and 0<=newc<n and distances[newr][newc]> max(dist, abs(heights[r][c] - heights[newr][newc])):
-                distances[newr][newc] = max(dist, abs(heights[r][c] - heights[newr][newc]))
-                queue.append((newr, newc, distances[newr][newc]))
-      return distances[m-1][n-1]
+        # dijkstra
+        m,n =len(heights), len(heights[0])
+        pq = []
+        dist = [[float('inf')]*n for i in range(m)]
+        pq.append((0,0,0))
+        while pq:
+            wt, i, j = heapq.heappop(pq)
+            if i==m-1 and j==n-1:
+                return wt
+            directions = {(i+1, j), (i, j+1), (i-1, j), (i, j-1)}
+            for newi, newj in directions:
+                if 0<=newi<m and 0<=newj<n and dist[newi][newj] > max(wt,abs(heights[i][j] - heights[newi][newj])):
+                    dist[newi][newj] = max(wt,abs(heights[i][j] - heights[newi][newj]))
+                    heapq.heappush(pq,(max(wt,abs(heights[i][j] - heights[newi][newj])), newi, newj))
+        return -1
